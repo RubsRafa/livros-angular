c95b611e36433da43137ec3a5854245fefdd9251
RubsRafa <rubiarafaela2000@gmail.com>
2024-05-23 13:59:11 -0300

    feat: add providers to main app component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { LivroListaComponent } from './app/livro-lista/livro-lista.component';
+import { LivroDadosComponent } from './app/livro-dados/livro-dados.component';
+import ControleEditoraService from './app/controle-editora.service';
+import ControleLivrosService from './app/controle-livros.service';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: 'lista', component: LivroListaComponent },
+      { path: 'dados', component: LivroDadosComponent },
+      { path: '', redirectTo: '/lista', pathMatch: 'full' },
+    ]),
+    ControleEditoraService,
+    ControleLivrosService,
+    provideHttpClient(),
+  ],
+}).catch((err) => console.error(err));
